b443acd8bfbbe4502770ff6dce9c133fbcbecaa5
JamaHCS <jama117@hotmail.com>
2020-07-13 20:21:16 -0500

    init conf

---
+++ database/factories/SellFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\sell;
+use Faker\Generator as Faker;
+
+$factory->define(sell::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
