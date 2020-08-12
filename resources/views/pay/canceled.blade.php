86ad98d73ef62c0cc78c727b0d8719675c7b10a3
JamaHCS <jama117@hotmail.com>
2020-08-12 14:19:26 -0500

    fast estate return of order

--- resources/views/pay/canceled.blade.php
+++ resources/views/pay/canceled.blade.php
@@ -2,7 +2,8 @@
 @section('title', 'Compra cancelada')
 
 @section('content')
-    <div class="mx-5">
-      <h1>CANCELADA</h1>
-    </div>
+<div class="mx-5" style="text-align: center">
+  <h1 style="text-align: center" class="py-4">CANCELADA</h1>
+  <h4 style="text-align: center">Tu compra ha sido cancelada ):</h4>
+</div>
 @endsection
